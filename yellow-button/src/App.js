d79ed57086c27d6fe62752a9bbc39607cafe1f4a
Maksim Yudzeshka <yudzeshka@gmail.com>
2021-12-08 16:52:50 +0300

    Create new app, add button, add setInterval function

---
+++ yellow-button/src/App.js
@@ -0,0 +1,50 @@
+import "./App.css";
+
+import React, { Component } from "react";
+import Button from "./Button";
+
+const colors = [
+  "Lime",
+  "MediumVioletRed",
+  "Tomato",
+  "Magenta",
+  "Aqua",
+  "Yellow",
+];
+
+function randomNumber(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+export class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      color: "DarkCyan",
+    };
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        color: randomNumber(colors),
+      });
+    }, 1000);
+  }
+
+  render() {
+    const { color } = this.state;
+
+    return (
+      <button style={{ backgroundColor: color, padding: "10px" }}>
+        text="I am button"
+      </button>
+    );
+  }
+}
+
+export default App;
+
+// function randomNumber(arr) {
+//   return arr[Math.floor(Math.random() * arr.length)];
+// }
